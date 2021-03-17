115f7884ff589fd676d9e34dc999146ce99b0299
K4sik <kasiks597@gmail.com>
2021-03-17 22:23:32 +0200

    Added Document && DocumentType Entities

--- src/main/java/com/kas/security_agency/entity/Document.java
+++ src/main/java/com/kas/security_agency/entity/Document.java
@@ -1,2 +1,38 @@
-package com.kas.security_agency.entity;public class Document {
+package com.kas.security_agency.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Document")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "type_of_document", nullable = false)
+    private DocumentType documentType;
+
+    @NotBlank(message = "Number cannot be blank")
+    private String number;
+
+    //    @NotBlank(message = "Date of Birth cannot be blank")
+    private Date date_of_issue;
+
 }
--- src/main/java/com/kas/security_agency/entity/DocumentType.java
+++ src/main/java/com/kas/security_agency/entity/DocumentType.java
@@ -1,2 +1,31 @@
-package com.kas.security_agency.entity;public class DocumentType {
+package com.kas.security_agency.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "DocumentType")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DocumentType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @OneToMany(mappedBy = "documentType", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Document> documents;
 }
