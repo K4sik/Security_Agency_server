f9538512c13a7edcab6fea4c280a27cb78093423
K4sik <kasiks597@gmail.com>
2021-03-18 12:39:12 +0200

    Added Product and ListOfProduct Entities

---
+++ src/main/java/com/kas/security_agency/entity/ListOfProduct.java
@@ -0,0 +1,24 @@
+package com.kas.security_agency.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ListOfProduct")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ListOfProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+}
---
+++ src/main/java/com/kas/security_agency/entity/Product.java
@@ -0,0 +1,36 @@
+package com.kas.security_agency.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "Product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Name cannot be blank")
+    private double price;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String provider;
+
+
+
+}
