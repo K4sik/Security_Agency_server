20908216be719e240f967c5b10f6adc7505c987c
K4sik <kasiks597@gmail.com>
2021-03-19 17:18:29 +0200

    Updated and Added DocumentType, Document Repositories, Services and Exceptions

--- src/main/java/com/kas/security_agency/repository/DocumentTypeRepository.java
+++ src/main/java/com/kas/security_agency/repository/DocumentTypeRepository.java
@@ -4,6 +4,11 @@ import com.kas.security_agency.entity.DocumentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
+
+    Optional<DocumentType> getById(Long id);
+
 }
